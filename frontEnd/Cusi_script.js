f725048fb41c0534a9ceb4105c73805529c53083
Alumno Suizo <alumno@ET26.DOMINIO>
2026-08-11 10:44:29 -0300

    comienzo del efecto de sonido

--- frontEnd/Cusi_script.js
+++ frontEnd/Cusi_script.js
@@ -1,18 +1,28 @@
-  async function irA(url) {
-        //alert(url);
-            
-            try {
-                const response = await fetch(url, {
-                    method: 'POST' 
-                });
+async function irA(url) {
+    try {
+        const response = await fetch(url, {
+            method: 'POST'
+        });
 
-                if (!response.ok) throw new Error('Error en la petición');
+        if (!response.ok) throw new Error('Error en la petición');
 
-                const html = await response.text();
-                
-                document.getElementById("content").innerHTML = html;
+        const html = await response.text();
 
-            } catch (error) {
-                console.error('Hubo un error:', error);
-            }    
-        }  //php a pelo
+        document.getElementById("content").innerHTML = html;
+
+    } catch (error) {
+        console.error('Hubo un error:', error);
+    }
+}
+
+
+const cusi = document.getElementById("cusi_a")
+const deam = document.getElementById("damn")
+function cusiQ() {
+    cusi.addEventListener('click', function(){
+        deam.currentTime = 0;
+        deam.play().catch(error=>{
+            console.log("no funciona");
+        })
+    })
+}
--- frontEnd/backEnd/living.php
+++ frontEnd/backEnd/living.php
@@ -19,9 +19,11 @@
       </div>
 
       <img src="/CUSI-1.0/frontEnd/Cusi_style/CUSI_skins/Cusi.png" width="550" id="cusi_a">
+      <audio src="/CUSI-1.0/frontEnd/Deam.mp3" id="damn"></audio> 
       <img src="/CUSI-1.0/frontEnd/Cusi_style/living-scr/tienda_btn.png" width="350" id="tienda" class="im-btn" style="cursor: pointer;">
       <img src="/CUSI-1.0/frontEnd/Cusi_style/CUSI_skins/Diego.png" width="350" id="diego">
    </section>
+
 
    <script src="Cusi_script.js"></script>
 </body>
