a12f1b4d87a561cb05f4151fcf025a37007969a1
Eduardo <eduardovasquezguerra@gmail.com>
2026-08-18 12:20:30 -0300

    html provisional

---
+++ html/scrips.js
@@ -0,0 +1,17 @@
+  async function irA(url) {
+            
+            try {
+                const response = await fetch(url, {
+                    method: 'POST' 
+                });
+
+                if (!response.ok) throw new Error('Error en la petición');
+
+                const html = await response.text();
+                
+                document.getElementById("content").innerHTML = html;
+
+            } catch (error) {
+                console.error('Hubo un error:', error);
+            }    
+        } 
