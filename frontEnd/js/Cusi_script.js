e0ccfca7e7e0f7257d26088562ee923b8f05c8ee
GitRayaCR <rayaleon0926@gmail.com>
2026-08-29 23:14:43 -0300

    ahora gime cuando lo tocas

--- frontEnd/js/Cusi_script.js
+++ frontEnd/js/Cusi_script.js
@@ -15,12 +15,20 @@ async function irA(url) {
     }
 }
 
-document.addEventListener('click', function (e) {
-  console.log('clickeaste:', e.target.tagName, e.target.id, e.target.className);
 
-  if (e.target.id === 'cusi_a') {
-    const deam = document.getElementById("damn");
-    deam.currentTime = 0;
-    deam.play().catch(error => console.log('no funciona', error));
-  }
+document.addEventListener('click', function (e) {
+    if (e.target.id == 'cusi_a') {
+        const deam = document.getElementById("damn");
+        const cusi = document.getElementById("cusi_a");
+
+        deam.currentTime = 0;
+        deam.play().catch(error => console.log('no funciona', error));
+
+        cusi.src = "/CUSI-1.0/frontEnd/Cusi_style/CUSI_skins/Cusi_enojado.png";
+
+        // Vuelve a los 1000ms (1 segundo)
+        setTimeout(() => {
+            cusi.src = "/CUSI-1.0/frontEnd/Cusi_style/CUSI_skins/Cusi.png";
+        }, 700);
+    }
 });
