32d7eb8d9e7f475ccbfb8cfe1216dbf49e104504
GitRayaCR <rayaleon0926@gmail.com>
2026-08-29 21:40:22 -0300

    corregí algo del audio de deam

--- frontEnd/Cusi_script.js
+++ frontEnd/Cusi_script.js
@@ -1,18 +1,26 @@
-  async function irA(url) {
-        //alert(url);
-            
-            try {
-                const response = await fetch(url, {
-                    method: 'POST' 
-                });
+async function irA(url) {
+    try {
+        const response = await fetch(url, {
+            method: 'POST'
+        });
 
-                if (!response.ok) throw new Error('Error en la petición');
+        if (!response.ok) throw new Error('Error en la petición');
 
-                const html = await response.text();
-                
-                document.getElementById("content").innerHTML = html;
+        const html = await response.text();
 
-            } catch (error) {
-                console.error('Hubo un error:', error);
-            }    
-        }  //php a pelo
+        document.getElementById("content").innerHTML = html;
+
+    } catch (error) {
+        console.error('Hubo un error:', error);
+    }
+}
+
+document.addEventListener('click', function (e) {
+  console.log('clickeaste:', e.target.tagName, e.target.id, e.target.className);
+
+  if (e.target.id == 'cusi_a') {
+    const deam = document.getElementById("damn");
+    deam.currentTime = 0;
+    deam.play().catch(error => console.log('no funciona', error));
+  }
+});
