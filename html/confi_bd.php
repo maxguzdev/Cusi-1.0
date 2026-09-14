feef7185e77a59ac9aa8e76ff7c3e75fe29d7add
Alumno Suizo <alumno@ET26.DOMINIO>
2026-09-14 11:58:51 -0300

    nuevo carajo

--- html/confi_bd.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-session_start();
-
-echo "Imagen: " . $_SESSION['img'] . "<br>";
-echo "Nombre de perfil: " . $_SESSION['nombre_perfil'] . "<br>";
-echo "Biografía: " . $_SESSION['bio'] . ".";
-?>
