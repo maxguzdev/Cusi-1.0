b30e7d39b7a19a426b810be7f406ff3af865b024
maxguzdev <guzmanmaximopereyra@gmail.com>
2026-09-17 00:06:38 -0300

    ya esta todo solo falta un poco de css y q leon lo retoque uwu

--- minijuegoss/tragaperras/Juego_Tragaperras-main/assets/javascript/script.js
+++ minijuegoss/tragaperras/Juego_Tragaperras-main/assets/javascript/script.js
@@ -2,12 +2,12 @@
 Copyright (c) 2024. Alejandro Alberto Jiménez Brundin
 =======================================================*/
 
-let creditos = 0;
+let creditos = 67;
 
 function insertarMonedas() {
   creditos++;
   document.getElementById("cuentaCreditos").innerHTML = creditos + "<span style='color: red; font-size: smaller;'> +1 </span>";
-  document.getElementById("mensaje").innerHTML = "Has añadido 1 crédito.";
+  document.getElementById("mensaje").innerHTML = "Let's Go Gambling";
 }
 
 function rand(n) {
@@ -16,7 +16,7 @@ function rand(n) {
 
 function tirar() {
   if (creditos === 0) {
-    document.getElementById("mensaje").innerHTML = "No tienes créditos.";
+    document.getElementById("mensaje").innerHTML = "Oh no perdiste tu casa, Roba dinero y Vuelve.";
     return;
   }
 
@@ -57,19 +57,69 @@ function tirar() {
   }, 25);
 }
 
+const sonidoGanar = new Audio('./assets/song/win.mp3');
+const sonidoPerder = new Audio('./assets/song/lose2.mp3');
 
 function verificarResultados(slot1, slot2, slot3) {
+  let mensajeElemento = document.getElementById("mensaje");
+
   if (slot1 === slot2 && slot2 === slot3) {
-    document.getElementById("mensaje").innerHTML =
-      "¡Felicidades! Has ganado. :) Tienes: " + creditos + " monedas";
+    sonidoGanar.currentTime = 0;
+    sonidoGanar.play().catch(error => console.log("Error al reproducir audio de ganar:", error));
+
+    setTimeout(() => {
+      sonidoGanar.pause();
+      sonidoGanar.currentTime = 0;
+    }, 3000);
+
+    mensajeElemento.innerHTML =
+      "Carajo, Ganaste Vuelve a apostar Seguro lo multiplicas!!! Tienes: " + creditos + " monedas";
     creditos = creditos + 10;
   } else {
-    document.getElementById("mensaje").innerHTML =
-      "Has perdido. :( Tienes: " + creditos + " monedas";
-    creditos--;
+    sonidoPerder.currentTime = 0;
+    sonidoPerder.play().catch(error => console.log("Error al reproducir audio de perder:", error));
+
+    setTimeout(() => {
+      sonidoPerder.pause();
+      sonidoPerder.currentTime = 0;
+    }, 3000);
+
+    mensajeElemento.innerHTML =
+      "Oh no Perdiste, Siempre hay una Monedita de mas. Tienes: " + creditos + " monedas";
+    if (creditos > 0) {
+      creditos--;
+    }
   }
   document.getElementById("cuentaCreditos").innerHTML = creditos;
 }
+
+const sonidoInicio = new Audio('./assets/song/maquinitainicio.mp3');
+
+document.getElementById('botonTirar').addEventListener('click', () => {
+  sonidoInicio.currentTime = 0; 
+  sonidoInicio.play().catch(error => {
+    console.log("No Funciono:", error);
+  });
+
+  setTimeout(() => {
+    sonidoInicio.pause();
+    sonidoInicio.currentTime = 0;
+  }, 4000);
+});
+
+const sonidocoin = new Audio('./assets/song/moneda.mp3');
+
+document.getElementById('insertarMoneda').addEventListener('click', () => {
+  sonidocoin.currentTime = 0; 
+  sonidocoin.play().catch(error => {
+    console.log("No Funciono:", error);
+  });
+
+  setTimeout(() => {
+    sonidocoin.pause();
+    sonidocoin.currentTime = 0;
+  }, 1000);
+});
 
 /*=======================================================
 Copyright (c) 2024. Alejandro Alberto Jiménez Brundin
